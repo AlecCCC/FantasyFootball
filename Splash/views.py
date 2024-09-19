20b6d0d542614064d964188a0f6498a00d22c495
Alec <alecjames1996@aol.com>
2024-09-19 02:50:07 -0400

    Progress on splash page -changed view to get league photo -css styles -display league photo -use css grid layout for cards. -media query for smaller screen to add some margin at the bottom.

--- Splash/views.py
+++ Splash/views.py
@@ -1,7 +1,12 @@
 import requests
 from django.shortcuts import render
 
+import requests
+from django.shortcuts import render
+
+
 def splash_view(request):
+
     if request.method == 'POST':
         username = request.POST.get('username')
         user_response = requests.get(f'https://api.sleeper.app/v1/user/{username}')
@@ -13,6 +18,17 @@ def splash_view(request):
 
             if leagues_response.status_code == 200:
                 leagues_data = leagues_response.json()
+
+
+                for league in leagues_data:
+                    avatar_id = league.get('avatar')
+                    if avatar_id:
+
+                        league['avatar_url'] = f"https://sleepercdn.com/avatars/{avatar_id}"
+                    else:
+                        league['avatar_url'] = None
+
+
                 return render(request, 'splash.html', {'leagues': leagues_data})
             else:
                 # Handle case where leagues API request fails
@@ -21,4 +37,5 @@ def splash_view(request):
             # Handle case where user API request fails
             return render(request, 'splash.html', {'error': 'Failed to fetch user'})
 
-    return render(request, 'splash.html')
+    context = {}
+    return render(request, 'splash.html', context)

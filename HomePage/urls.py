393b985af24fd562c434941509976fac1bc7247c
Alec <alec.martin@goqwickly.com>
2024-09-25 10:43:14 -0400

    Incorporated weekly matchups view to start current week, currently need to make carousel component to show different weeks.

--- HomePage/urls.py
+++ HomePage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:league_id>/index/', views.index, name='index'),
-    path('<int:league_id>/matchups/', views.matchups, name='matchups')
+    path('<int:league_id>/matchups/<int:current_week>', views.matchups, name='matchups')
 ]

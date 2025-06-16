6494068dc277260ac18bb713881270aeed24a8c9
Alec <alecjames1996@aol.com>
2025-06-15 20:49:14 -0400

    changed settings.py

--- FantasyLeague/settings.py
+++ FantasyLeague/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+6gh#8_yk8^u%@s2+&o-%bwfa0++(s8dgdege-y*6kfco(7v1q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['sleeperapi.herokuapp.com', 'sleeperapi-d168cb1e1349.herokuapp.com']
+ALLOWED_HOSTS = ['sleeperapi.herokuapp.com', 'sleeperapi-d168cb1e1349.herokuapp.com',"mysleeperapi.com", "www.mysleeperapi.com"]
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://mysleeperapi.com',
+    'https://mysleeperapi.com', "https://sleeperapi-d168cb1e1349.herokuapp.com","https://www.mysleeperapi.com"
 ]
 
 MIDDLEWARE = [

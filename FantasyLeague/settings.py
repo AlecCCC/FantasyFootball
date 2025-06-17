c889c2cd886d98134acb8c60c388cbeb0ca477cb
Alec <alecjames1996@aol.com>
2025-06-17 02:44:00 -0400

    changed css links in html pages and changed key... oopsie

--- FantasyLeague/settings.py
+++ FantasyLeague/settings.py
@@ -12,17 +12,20 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-#from decouple import config
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+6gh#8_yk8^u%@s2+&o-%bwfa0++(s8dgdege-y*6kfco(7v1q'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
